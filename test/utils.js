f5ddb6f875277c66bfdea824b40dbf42134b8237
Guillaume Wuip <guillaume@wuips.com>
2016-06-22 21:39:55 +0200

    Better utils test

--- test/utils.js
+++ test/utils.js
@@ -9,9 +9,18 @@
 
         const utils = require('../lib/utils');
 
-        it('should have addLockedProperty property', function () {
+        it('should have addLockedProperty property', function (done) {
             assert.property(utils, 'addLockedProperty');
             assert.typeOf(utils.addLockedProperty, 'function');
+
+            const obj = {};
+            utils.addLockedProperty(obj, 'p', 1234);
+            try {
+                obj.p = 5678;
+                done(false);
+            } catch(e) {
+                done();
+            }
         });
 
     });
