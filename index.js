0533ed44fe46c9d5d9f70b9385306253fa657012
Guillaume Wuip <guillaume@wuips.com>
2016-06-22 21:08:55 +0200

    Remove constants.js

--- index.js
+++ index.js
@@ -4,21 +4,27 @@
 (() => {
 
     const
+        utils       = require('./lib/utils'),
         request     = require('./lib/request'),
-        status      = require('./lib/status'),
-        constants   = require('./lib/constants');
+        status      = require('./lib/status');
 
-    module.exports = Object.assign(
-        {},
-        constants,
-        {
-            request: request,
-            get: (id) => {
-                return request(id).then((res) => {
-                    return res.hasData ? status(res) : new Error('no data');
-                });
-            },
-        }
-    );
+    const rain = {};
+
+    //add constants
+    utils.addLockedProperty(rain, 'NO_DATA',         0);
+    utils.addLockedProperty(rain, 'NO_RAIN',         1);
+    utils.addLockedProperty(rain, 'LIGHT_RAIN',      2);
+    utils.addLockedProperty(rain, 'MODERATE_RAIN',   3);
+    utils.addLockedProperty(rain, 'HEAVY_RAIN',      4);
+
+    rain.request = request;
+
+    rain.get = (id) => {
+            return request(id).then((res) => {
+            return res.hasData ? status(res) : new Error('no data');
+        });
+    };
+
+    module.exports = rain;
 
 })();
