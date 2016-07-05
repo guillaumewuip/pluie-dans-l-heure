5bd7e3ebbcf39aafccd83a5cba81be8a933cfa05
Guillaume Wuip <guillaume@wuips.com>
2016-07-05 08:15:11 +0200

    Add Typescript module definition

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+
+enum RAIN_TYPE {
+    NO_DATA,
+    NO_RAIN,
+    LIGHT_RAIN,
+    MODERATE_RAIN,
+    HEAVY_RAIN,
+}
+
+interface Res {
+    raw: any;
+    idLieu: string;
+    lastUpdate: Date;
+    validity: Date;
+    raining: boolean;
+    willRain: boolean;
+    windows: number[];
+    confidence: number;
+}
+
+export const NO_DATA: RAIN_TYPE;
+export const NO_RAIN: RAIN_TYPE;
+export const LIGHT_RAIN: RAIN_TYPE;
+export const MODERATE_RAIN: RAIN_TYPE;
+export const HEAVY_RAIN: RAIN_TYPE;
+
+export const get: (id: string) => Promise<Res>;
+
